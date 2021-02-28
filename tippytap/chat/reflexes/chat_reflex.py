2e71bf290ee535ebebcf93724454aef2b5c726db
Huon Imberger <huon@plonq.org>
2021-02-28 19:53:36 +1100

    Loop sessions

--- tippytap/chat/reflexes/chat_reflex.py
+++ tippytap/chat/reflexes/chat_reflex.py
@@ -1,3 +1,4 @@
+from django.contrib.sessions.models import Session
 from django.template.loader import render_to_string
 from sockpuppet.channel import Channel
 from channels.layers import get_channel_layer
@@ -17,13 +18,10 @@ class ChatReflex(Reflex):
         message = Message.objects.create(text=text, user_id=user_id)
 
         session_key = self.session.session_key
-        groups = get_channel_layer().groups
-        for group in groups:
-            if group == session_key or group == 'StimulusReflex-Channel':
-                continue
-
+        session_keys = Session.objects.exclude(session_key=session_key).values_list("session_key", flat=True)
+        for key in session_keys:
             html = render_to_string("chat_message.html", {"message": message})
-            channel = Channel(group, identifier='{"channel":"StimulusReflex::Channel"}')
+            channel = Channel(key, identifier='{"channel":"StimulusReflex::Channel"}')
             channel.insert_adjacent_html({
                 "selector": ".chat .conversation",
                 "position": "afterbegin",
