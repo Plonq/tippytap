201bf6c4edae3a485e28f6fd018799b653986eab
Huon Imberger <huon@plonq.org>
2021-02-28 19:35:00 +1100

    More efficient message broadcast

--- tippytap/chat/reflexes/chat_reflex.py
+++ tippytap/chat/reflexes/chat_reflex.py
@@ -1,4 +1,3 @@
-from django.contrib.sessions.models import Session
 from django.template.loader import render_to_string
 from sockpuppet.channel import Channel
 from channels.layers import get_channel_layer
@@ -14,14 +13,20 @@ class ChatReflex(Reflex):
         pass
 
     def new_message(self, text):
-        print("new_message", text)
         user_id = self.session.get("user_id", None)
-        Message.objects.create(text=text, user_id=user_id)
+        message = Message.objects.create(text=text, user_id=user_id)
 
-        # Force update of all users
-        channel = Channel('StimulusReflex-Channel', identifier='{"channel":"StimulusReflex::Channel"}')
-        channel.dispatch_event({
-            "name": "force:update",
-            "selector": ".chat",
-        })
-        channel.broadcast()
+        session_key = self.session.session_key
+        groups = get_channel_layer().groups
+        for group in groups:
+            if group == session_key or group == 'StimulusReflex-Channel':
+                continue
+
+            html = render_to_string("chat_message.html", {"message": message})
+            channel = Channel(group, identifier='{"channel":"StimulusReflex::Channel"}')
+            channel.insert_adjacent_html({
+                "selector": ".chat .conversation",
+                "position": "afterbegin",
+                "html": html,
+            })
+            channel.broadcast()
